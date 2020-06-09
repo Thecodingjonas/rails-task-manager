beddd3b1c9a9091805f5caa69d9de41472b46239
jonas fürstenberg <jonas.fuerstenberg99@gmail.com>
2020-06-09 23:11:08 +0800

    A nice looking task manager

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+// new_task.addEventListener((event)=> {
+//   window.alert('You out here being organized')
+// })
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/tasks', to: 'tasks#index'
+
+  get '/task/new', to: 'tasks#new', as: 'task_new'
+
+  post '/tasks', to: 'tasks#create'
+
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  patch '/tasks/:id', to: 'tasks#update', as: 'task_update'
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
+
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
